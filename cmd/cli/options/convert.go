57a1dd5ffe6416672499fb4e1ae76cadbc8801a9
Wessel Haze <wesselhazeamerongen@gmail.com>
2025-03-11 20:24:49 +0100

    Add the cyclonedx-1.6 format option
    Signed-off-by: Wessel Haze <wesselhazeamerongen@gmail.com>

--- cmd/cli/options/convert.go
+++ cmd/cli/options/convert.go
@@ -13,7 +13,7 @@ type ConvertOptions struct {
 
 // AddFlags adds command line flags for the ConvertOptions struct
 func (o *ConvertOptions) AddFlags(cmd *cobra.Command) {
-	cmd.Flags().StringVarP(&o.Format, "format", "f", "", "the output format [spdx, spdx-2.3, cyclonedx, cyclonedx-1.0, cyclonedx-1.1, cyclonedx-1.2, cyclonedx-1.3, cyclonedx-1.4, cyclonedx-1.5]") //nolint: lll
+	cmd.Flags().StringVarP(&o.Format, "format", "f", "", "the output format [spdx, spdx-2.3, cyclonedx, cyclonedx-1.0, cyclonedx-1.1, cyclonedx-1.2, cyclonedx-1.3, cyclonedx-1.4, cyclonedx-1.5, cyclonedx-1.6]") //nolint: lll
 	cmd.Flags().StringVarP(&o.Encoding, "encoding", "e", "json", "the output encoding [spdx: [text, json] cyclonedx: [json]")
 	cmd.Flags().StringVarP(&o.OutputPath, "output", "o", "", "path to write the converted SBOM")
 }
